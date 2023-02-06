09d2aee0d4086036601b53d9e1b22eb8de8bb53a
Harut101 <arutaydinyan@gmail.com>
2023-02-06 21:23:43 +0400

    finalize

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Card({ title, text, target, linkTitle, href, rel, linkClassName, onClick }) {
   return (
     <div className="card">
-      <div className="card__title">{title}</div>
+      <p className="card__title">{title}</p>
 
       <div className="card__text">{text}</div>
       <a className={`default-link card__link ${linkClassName}`} target={target} rel={rel} href={href} onClick={() => onClick(href)}>
